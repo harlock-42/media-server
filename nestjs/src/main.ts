59e35e85d8dae8870f0ebff273e58cf20a9c9795
harlock-42 <allaire.terry.at@gmail.com>
2024-10-08 20:01:19 +0200

    init video storage path

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { existsSync, mkdirSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const mediaPath = process.env.MEDIA_PATH || '/app/media';
+  const videoDir = join(mediaPath, 'videos');
+
+  if (!existsSync(videoDir)) {
+    mkdirSync(videoDir, { recursive: true });
+    console.log(`Repository ${videoDir} created`);
+  }
   await app.listen(3000);
 }
 bootstrap();
